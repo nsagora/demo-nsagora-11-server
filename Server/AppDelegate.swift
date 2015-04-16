b512aac1d3b8a70f6f3cb62fe0f90326c36d2566
Ilinca <ilinca.ispas@gmail.com>
2015-04-16 18:00:28 +0300

    Initial Commit

---
+++ Server/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Server
+//
+//  Created by Ilinca on 16/04/2015.
+//  Copyright (c) 2015 iosnsagora. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Server/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Server
+//
+//  Created by Ilinca on 16/04/2015.
+//  Copyright (c) 2015 iosnsagora. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
